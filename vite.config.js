5b21f6707e46a5dcbd2ba611bc1628ba9f32a5a6
umutgueler <umutgulerr14@gmail.com>
2023-01-22 20:27:58 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     https: false,
     proxy: {
       '/api': {
-        target: 'http://localhost:6600',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\//, ""),
